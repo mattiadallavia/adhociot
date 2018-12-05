4f714346dda99dfc6e9db0843e7d5706852bb107
Mattia Dalla Via <mattia@dallavia.it>
2018-12-05 16:36:18 +0100

    Refactored rout

--- rout.c
+++ rout.c
@@ -32,7 +32,7 @@ struct node
 	int messages_len;
 };
 
-struct statistics
+struct stat
 {
 	long t;
 	long tx;
@@ -40,7 +40,7 @@ struct statistics
 	long collisions;
 };
 
-struct statistics alg(struct node *nodes, float *graph, int n);
+void disp(struct stat *st, struct node *nodes, float *graph, int n);
 struct message peek(struct node *n);
 int find(struct node *n, int number);
 void push(struct node *n, struct message m);
@@ -49,6 +49,7 @@ void read_graph(float *graph, int n);
 void print_nodes(struct node *nodes, int n);
 
 static int flag_steps = 0;
+static int flag_act = 0;
 static int flag_coll = 0;
 static int bfac = 5;
 static float wfac = 3;
@@ -56,6 +57,7 @@ static float wfac = 3;
 static struct option long_options[] =
 {
     {"steps",      no_argument,       &flag_steps, 1},
+    {"actions",    no_argument,       &flag_act,   1},
     {"collisions", no_argument,       &flag_coll,  1},
     {"wfactor",    required_argument, 0,         'w'},
     {"bfactor",    required_argument, 0,         'b'},
@@ -64,6 +66,7 @@ static struct option long_options[] =
 
 // usage: ./rout
 //  --steps         print intermediate steps
+//  --actions       print nodes actions
 //  --collisions    enable collision detection and avoidance
 //  --wfactor W     set wait factor
 //  --bfactor B     set branching factor
@@ -72,9 +75,10 @@ int main(int argc, char **argv)
 {
 	int i, opt;
 	int n, env, range, seed, conn;
+	int dis, dis_t;
+	struct stat st;
 	float *graph;
 	struct node *nodes;
-	struct statistics stats;
 
 	// optional arguments
 	while ((opt = getopt_long(argc, argv, "w:b:", long_options, 0)) != -1)
@@ -98,6 +102,7 @@ int main(int argc, char **argv)
 	srand(seed);
 	graph = malloc((n+1) * (n+1) * sizeof (float));
 	nodes = calloc((n+1), sizeof (struct node));
+	memset(&st, 0, sizeof (struct stat));
 
 	read_graph(graph, n+1);
 
@@ -117,172 +122,177 @@ int main(int argc, char **argv)
 	nodes[0].depth = 0;
 	nodes[0].channel = 0;
 
-	stats = alg(nodes, graph, n+1);
-
-	printf("%ld\t%ld\t%ld\t%ld\n", stats.t, stats.tx, stats.rx, stats.collisions);
-}
-
-struct statistics alg(struct node *nodes, float *graph, int n)
-{
-	int i, pos, coll, disp, disp_t;
-	int i_tx, i_rx;
-	float dist;
-	struct node *n_tx, *n_rx;
-	struct message m_tx, m_rx;
-	struct statistics stats;
-	memset(&stats, 0, sizeof (struct statistics));
-
-	while (1) // time loop
+	do
 	{
-		disp = 0;
-		disp_t = 0;
+		dis = dis_t = 0;
 
 		// check every active node for messages to dispatch
-		for (i = 0; i < n; i++) if ((nodes[i].state == NODE_ACTIVE) && (nodes[i].messages_len > 0))
+		for (i = 0; i < (n+1); i++) if ((nodes[i].state == NODE_ACTIVE) &&
+			                            (nodes[i].messages_len > 0))
 		{
-			disp++; // count nodes with messages to dispatch, now or in the future
+			dis++; // count nodes with messages to dispatch, now or in the future
 
 			// if it is the right slot for this node
 			// and is scheduled for this (or a past) time
-			if (((!flag_coll) || (SLOT(stats.t) == (2 - nodes[i].depth % 3))) &&
-				(stats.t >= nodes[i].wait))
+			if (((!flag_coll) || (SLOT(st.t) == (2 - nodes[i].depth % 3))) &&
+				(st.t >= nodes[i].wait))
 			{
-				disp_t++; // messages to dispatch in this t
+				dis_t++; // nodes with messages to dispatch during current t
 				nodes[i].transmitting = 1;
 			}
 		}
 
-		if (disp == 0) return stats; // there are no more messages to dispatch
-		if (disp_t == 0) // no mess. to disp. in this t
+		if (flag_steps && dis_t)
 		{
-			stats.t++;
-			continue;
-		}
-
-		if (flag_steps)
-		{
-			printf("t=%ld (frame %ld, slot %ld)\n", stats.t, FRAME(stats.t), SLOT(stats.t));
+			printf("t=%ld (frame %ld, slot %ld)\n", st.t, FRAME(st.t), SLOT(st.t));
 			print_nodes(nodes, n);
 		}
 
-		// dispatch all transmissions in t
-		for (i_tx = 0; i_tx < n; i_tx++) if (nodes[i_tx].transmitting)
-		{
-			n_tx = &nodes[i_tx];
-			m_tx = peek(n_tx);
+		disp(&st, nodes, graph, n+1);
 
-			m_rx.number = m_tx.number;
-			m_rx.node_confirm = i_tx;
-			m_rx.channel_confirm = n_tx->channel;
+		if (flag_steps && dis_t) printf("\n");
 
-			if (flag_steps) printf("node %d transmits message %d on channel %ld\n", i_tx, m_tx.number, n_tx->channel);
-
-			// transmit to all connected nodes
-			// node who are transmitting cannot receive at the same time
-			for (i_rx = 0; i_rx < n; i_rx++) if (IN_RANGE(i_tx, i_rx, graph, n) && !nodes[i_rx].transmitting)
-			{
-				n_rx = &nodes[i_rx];
-				dist = graph[i_tx+n*i_rx];
-
-				// collision detection
-				coll = 0;
-				if (flag_coll) for (i = 0; i < n; i++)
-				{
-					// is there anyone else connected to me transmitting on the same channel at the same time?
-					if (i != i_tx &&
-						nodes[i].transmitting &&
-						nodes[i].channel == n_tx->channel &&
-						IN_RANGE(i, i_rx, graph, n))
-					{
-						coll++;
-						stats.collisions++;
-						if (flag_steps) printf("collision with message %d from node %d at node %d\n", peek(&nodes[i]).number, i, i_rx);
-					}
-				}
-				// if a collision happened the message is not received
-				if (coll) continue;
-
-				// activate waiting nodes
-				if (n_rx->state == NODE_WAITING)
-				{
-					n_rx->state = NODE_ACTIVE;
-					if (flag_coll) n_rx->wait = stats.t + (int) (wfac*dist*10); // wait a frame proportional to the distance
-					if (flag_steps) printf("node %d activated, transmission scheduled for t=%ld\n", i_rx, n_rx->wait);
-				}
-
-				// update depths
-				if ((n_rx->depth < 0) || ((n_tx->depth + 1) < n_rx->depth)) // better path
-				{
-					n_rx->depth = n_tx->depth + 1;
-					n_rx->channel = -1;
-				}
-
-				// choose channel
-				if (flag_coll && n_tx->depth == n_rx->depth-1 &&
-					(n_rx->channel < 0 || n_rx->channel / bfac != n_tx->channel)) // p-1 node changed channel (or overflow)
-				{
-					n_rx->channel = bfac*n_tx->channel;
-					if (flag_steps) printf("node %d selects channel %ld\n", i_rx, n_rx->channel);
-				}
-
-				// select new channel if we receive an ack for another node transmitted on our channel
-				if (flag_coll && (n_tx->depth < n_rx->depth) && (m_tx.node_confirm != i_rx) && (m_tx.channel_confirm == n_rx->channel))
-				{
-					n_rx->channel++;
-					if (flag_steps) printf("channel %ld in use by node %d, node %d selects channel %ld\n", (n_rx->channel-1), m_tx.node_confirm, i_rx, n_rx->channel);
-				}
-
-				// ack for a message we sent
-				if ((n_tx->depth < n_rx->depth) && (m_tx.node_confirm == i_rx))
-				{
-					// reset the exp. backoff
-					n_rx->attempts = 0;
-					n_rx->wait = 0;
-					if (flag_steps) printf("node %d received an ack, transmission scheduled for t=%ld\n", i_rx, n_rx->wait);
-				}
-
-				// remove message from stack if we receive an ack from nearer the destination
-				// from us or another node, doesn't matter
-				if ((n_tx->depth < n_rx->depth) && ((pos = find(n_rx, m_rx.number)) >= 0))
-				{
-					delete(n_rx, pos);
-					if (flag_steps) printf("message %d removed from node %d\n", m_rx.number, i_rx);
-				}
-
-				// relay message if we are nearer the gateway
-				if ((n_tx->depth > n_rx->depth))
-				{
-					// remove old instances of the same message
-					if ((pos = find(n_rx, m_rx.number)) >= 0) delete(n_rx, pos);
-
-					push(n_rx, m_rx);
-					if (flag_steps) printf("added by node %d\n", i_rx);
-				}
-
-				stats.rx++;
-			}
-
-			// after transmitting
-			if (i_tx == 0) // if sink node
-			{
-				// remove sent message
-				delete(n_tx, find(n_tx, m_tx.number));
-			}
-			else
-			{
-				// delay transmissions unil sent message is acknowledged
-				n_tx->attempts++;
-				n_tx->wait = stats.t + rand() % (int)(wfac*pow(2, n_tx->attempts)) + 1; // exponential backoff
-				if (flag_steps) printf("node %d scheduled attempt n. %d at t=%ld\n", i_tx, (n_tx->attempts+1), n_tx->wait);
-			}
-
-			stats.tx++;
-		}
 		// end current transmissions
-		for (i = 0; i < n; i++) nodes[i].transmitting = 0;
+		for (i = 0; i < (n+1); i++) nodes[i].transmitting = 0;
 
-		stats.t++; // all messages dispatched for t, go to t+1
-		if (flag_steps) printf("\n");
+		st.t++; // all messages dispatched for t, go to t+1
+	}
+	while (dis);
+
+	printf("%ld\t%ld\t%ld\t%ld\n", st.t, st.tx, st.rx, st.collisions);
+}
+
+void disp(struct stat *st, struct node *nodes, float *graph, int n)
+{
+	int i, pos, coll;
+	int i_tx, i_rx;
+	float dist;
+	struct node *n_tx, *n_rx;
+	struct message m_tx, m_rx;
+
+	// dispatch all transmissions
+	for (i_tx = 0; i_tx < n; i_tx++) if (nodes[i_tx].transmitting)
+	{
+		n_tx = &nodes[i_tx];
+		m_tx = peek(n_tx);
+		m_rx.number = m_tx.number;
+		m_rx.node_confirm = i_tx;
+		m_rx.channel_confirm = n_tx->channel;
+
+		if (flag_act) printf("node %d transmits message %d on channel %ld\n", i_tx, m_tx.number, n_tx->channel);
+
+		// transmit to all connected nodes
+		// node who are transmitting cannot receive at the same time
+		for (i_rx = 0; i_rx < n; i_rx++) if (IN_RANGE(i_tx, i_rx, graph, n) &&
+		                                     !nodes[i_rx].transmitting)
+		{
+			n_rx = &nodes[i_rx];
+			dist = graph[i_tx+n*i_rx];
+
+			// collision detection
+			coll = 0;
+			if (flag_coll) for (i = 0; i < n; i++)
+			{
+				// is there anyone else connected to me transmitting
+				// on the same channel at the same time?
+				if (i != i_tx &&
+					nodes[i].transmitting &&
+					nodes[i].channel == n_tx->channel &&
+					IN_RANGE(i, i_rx, graph, n))
+				{
+					coll++;
+					st->collisions++;
+					if (flag_act) printf("collision with message %d from node %d at node %d\n", peek(&nodes[i]).number, i, i_rx);
+				}
+			}
+			// if a collision happened the message is not received
+			if (coll) continue;
+
+			// activate waiting nodes
+			if (n_rx->state == NODE_WAITING)
+			{
+				n_rx->state = NODE_ACTIVE;
+				// wait a frame proportional to the distance
+				if (flag_coll) n_rx->wait = st->t + (int) (wfac*dist*10);
+				if (flag_act) printf("node %d activated, transmission scheduled for t=%ld\n", i_rx, n_rx->wait);
+			}
+
+			// update depths
+			if (n_rx->depth < 0 ||
+			    n_tx->depth + 1 < n_rx->depth) // better path
+			{
+				n_rx->depth = n_tx->depth + 1;
+				n_rx->channel = -1;
+			}
+
+			// choose channel
+			// not initialized or p-1 node changed channel (or overflow)
+			if (flag_coll &&
+				n_tx->depth == n_rx->depth-1 &&
+				(n_rx->channel < 0 || n_rx->channel / bfac != n_tx->channel))
+			{
+				n_rx->channel = bfac*n_tx->channel;
+				if (flag_act) printf("node %d selects channel %ld\n", i_rx, n_rx->channel);
+			}
+
+			// select new channel if we receive an ack
+			// for another node transmitted on our channel
+			if (flag_coll &&
+				n_tx->depth < n_rx->depth &&
+				m_tx.node_confirm != i_rx &&
+				m_tx.channel_confirm == n_rx->channel)
+			{
+				n_rx->channel++;
+				if (flag_act) printf("channel %ld in use by node %d, node %d selects channel %ld\n", (n_rx->channel-1), m_tx.node_confirm, i_rx, n_rx->channel);
+			}
+
+			// ack for a message we sent
+			if (n_tx->depth < n_rx->depth &&
+				m_tx.node_confirm == i_rx)
+			{
+				// reset the exp. backoff
+				n_rx->attempts = 0;
+				n_rx->wait = 0;
+				if (flag_act) printf("node %d received an ack, transmission scheduled for t=%ld\n", i_rx, n_rx->wait);
+			}
+
+			// remove mess. from stack if we receive an ack from nearer the dest.
+			// from us or another node, doesn't matter
+			if (n_tx->depth < n_rx->depth &&
+				(pos = find(n_rx, m_rx.number)) >= 0)
+			{
+				delete(n_rx, pos);
+				if (flag_act) printf("message %d removed from node %d\n", m_rx.number, i_rx);
+			}
+
+			// relay message if we are nearer the gateway
+			if (n_tx->depth > n_rx->depth)
+			{
+				// remove old instances of the same message
+				if ((pos = find(n_rx, m_rx.number)) >= 0) delete(n_rx, pos);
+				push(n_rx, m_rx);
+				if (flag_act) printf("added by node %d\n", i_rx);
+			}
+
+			st->rx++;
+		}
+
+		// after transmitting
+		if (i_tx == 0) // if sink node
+		{
+			// remove sent message
+			delete(n_tx, find(n_tx, m_tx.number));
+		}
+		else
+		{
+			// delay transmissions unil sent message is acknowledged
+			n_tx->attempts++;
+			// exponential backoff
+			n_tx->wait = st->t + rand() % (int)(wfac*pow(2, n_tx->attempts)) + 1;
+			if (flag_act) printf("node %d scheduled attempt n. %d at t=%ld\n", i_tx, (n_tx->attempts+1), n_tx->wait);
+		}
+
+		st->tx++;
 	}
 }
 
