25ef46514340ec4717d70f4a7135b40692376622
Mattia Dalla Via <mattia@dallavia.it>
2018-12-05 02:57:01 +0100

    Added bfac to stat

--- stat.c
+++ stat.c
@@ -5,10 +5,14 @@
 #include <string.h>
 
 static int flag_coll = 0;
+static int flag_wfac = 0;
+static int flag_bfac = 0;
 
 static struct option long_options[] =
 {
     {"seed",       required_argument, 0,        's'},
+    {"wfactor",    required_argument, 0,        'w'},
+    {"bfactor",    required_argument, 0,        'b'},
     {"collisions", no_argument,       &flag_coll, 1},
     {0, 0, 0, 0}
 };
@@ -25,18 +29,28 @@ int main(int argc, char **argv)
 	int seed = time(0);
 	int n, n_min, n_max, n_step;
 	float dim, dim_min, dim_max, dim_step;
+	float w, wr, w_min = 1, w_max = 1, w_step = 1;
+	int b, br, b_min = 1, b_max = 1, b_step = 1;
 	int i, env, range, iter;
 	int conn, t, tx, rx, coll;
-	char command[100];
+	char comm[200];
 	FILE *pipe;
 
 	// optional arguments
-	while ((opt = getopt_long(argc, argv, "s:", long_options, 0)) != -1)
+	while ((opt = getopt_long(argc, argv, "s:w:b:", long_options, 0)) != -1)
 	{
 		switch (opt)
 		{
 			case 's':
 				seed = atoi(optarg);
+				break;
+			case 'w':
+				flag_wfac = 1;
+				sscanf(optarg, "%f %f %f", &w_min, &w_max, &w_step);
+				break;
+			case 'b':
+				flag_bfac = 1;
+				sscanf(optarg, "%d %d %d", &b_min, &b_max, &b_step);
 				break;
 			case '?':
 				return 1;
@@ -61,19 +75,29 @@ int main(int argc, char **argv)
 
 		for (n = n_min; n <= n_max; n += n_step)
 		{
-			// samples
-			for (i = 0; i < iter; i++)
+			for (b = b_min; b <= b_max; b += b_step)
 			{
-				sprintf(command, "./layout %d %d %d --seed %d | ./graph | ./sim",
-				        env, n, range, seed++);
-				if (flag_coll) strcat(command, " --collisions");
-				
-				pipe = popen(command, "r");
-				fscanf(pipe, "%*d\t%*d\t%*d\t%d\n", &conn);
-				fscanf(pipe, "%d\t%d\t%d\t%d\n", &t, &tx, &rx, &coll);
-				pclose(pipe);
+				for (w = w_min; w <= w_max; w += w_step)
+				{
+					// samples
+					for (i = 0; i < iter; i++)
+					{
+						sprintf(comm, "./layout %d %d %d --s %d | ./graph | ./sim",
+						        env, n, range, seed++);
+						if (flag_coll) strcat(comm, " --c");
+						if (flag_wfac) sprintf(comm+strlen(comm), " --w %f", w);
+						if (flag_bfac) sprintf(comm+strlen(comm), " --b %d", b);
 
-				printf("%f\t%d\t%d\t%d\t%d\t%d\t%d\n", dim, n, conn, t, tx, rx, coll);
+						pipe = popen(comm, "r");
+						fscanf(pipe, "%*d\t%*d\t%*d\t%*d\t%d\t%f\t%d\n", &conn, &wr, &br);
+						fscanf(pipe, "%d\t%d\t%d\t%d\n", &t, &tx, &rx, &coll);
+						pclose(pipe);
+
+						printf("%f\t%d\t%d\t%f\t%d\t%d\t%d\t%d\t%d\n", dim, n, conn, wr, br, t, tx, rx, coll);
+					}
+					if (flag_wfac) printf("\n");
+				}
+				if (flag_bfac) printf("\n");
 			}
 			printf("\n");
 		}
